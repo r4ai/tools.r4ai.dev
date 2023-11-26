de8eb0e14a7d9aee4bff976323e79007b9854f45
rai <mj82xx@gmail.com>
2023-11-26 14:42:56 +0900

    chore: huskyとlint-stagedを導入し、commit時にeslintとprettierを実行させる

---
+++ app/web/.eslintrc.cjs
@@ -0,0 +1,47 @@
+// @ts-check
+const { defineConfig } = require("eslint-define-config");
+
+/// <reference types="@eslint-types/import" />
+
+module.exports = defineConfig({
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "prettier",
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-undef": "off",
+  },
+  ignorePatterns: ["node_modules", "dist"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+    {
+      files: "*.cjs",
+      env: {
+        node: true,
+      },
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+});
